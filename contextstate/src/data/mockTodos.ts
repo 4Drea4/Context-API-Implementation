3b2632d60741d1f62f632c2f36ad28193ec7d0b5
4drea4 <bakerand@outlook.com>
2025-12-18 11:56:22 -0600

    Mock data

---
+++ contextstate/src/data/mockTodos.ts
@@ -0,0 +1,6 @@
+import type { Todo } from "../types";
+
+export const mockTodos: Todo[] =[
+    {id:55, text:"Go to the beach", completed: false},
+    {id:105, text:"Sweep sand on the beach", completed: true},
+];
