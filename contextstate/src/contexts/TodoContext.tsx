7778d40a343bed39160e9e91687da78795d66246
4drea4 <bakerand@outlook.com>
2025-12-18 12:02:07 -0600

    Todo context

--- contextstate/src/contexts/TodoContext.tsx
+++ contextstate/src/contexts/TodoContext.tsx
@@ -1,5 +1,15 @@
-import {createContext} from 'react';
+import {createContext, useContext} from 'react';
 import type { TodoContextType } from '../types';
 
 
-export const TodoContext = createContext<TodoContextType | null>(null);
+export const TodoContextValue = {
+    
+    todos: Todo[];
+    addTodo: (text:string) => void;
+    toggleTodo: (id:number) => void;
+    deleteTodo: (id: number) => void;
+    editTodo: (id: number, newText:string) => void;
+    clearCompleted: ()=> void;
+    
+};
+    export const TodoContext = createContext<TodoContextType | null>(null);
